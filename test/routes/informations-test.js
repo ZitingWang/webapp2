48ef595bc38dd5647f10e66b4a36dd88784abf07
ZITINGWANG <2264853546@qq.com>
2018-12-09 17:28:51 +0000

    update test

--- test/routes/informations-test.js
+++ test/routes/informations-test.js
@@ -5,20 +5,9 @@ let expect = chai.expect;
 import things from 'chai-things'
 chai.use( things);
 chai.use(chaiHttp);
+const request = require('supertest');
 let _ = require('lodash' );
 describe('Informations', function (){
-    describe('GET /informations',  () => {
-        it('should return all the informations in an array', function(done) {
-            chai.request(server)
-                .get('/informations')
-                .end(function(err, res) {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.be.a('array');
-                    expect(res.body.length).to.equal(0);
-                    done();
-                });
-        });
-    });
     describe('POST /informations', function () {
         it('should return confirmation message and update datastore', function(done) {
             let information = {
@@ -27,7 +16,7 @@ describe('Informations', function (){
                 sex: 'male',
                 amountofmessage: 0
             };
-            chai.request(server)
+            request(server)
                 .post('/informations')
                 .send(information)
                 .end(function(err, res) {
@@ -37,7 +26,7 @@ describe('Informations', function (){
                 });
         });
         after(function  (done) {
-            chai.request(server)
+            request(server)
                 .get('/informations')
                 .end(function(err, res) {
                     let result = _.map(res.body, (information) => {
@@ -49,9 +38,43 @@ describe('Informations', function (){
                 });
         });
     });
+    describe('GET /informations',  () => {
+        it('should return all the informations in an array', function(done) {
+            request(server)
+                .get('/informations')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(1);
+                    let result = _.map(res.body, (inf) => {
+                        return { id: inf.id,
+                            username: inf.username,
+                            sex: inf.sex}
+                    });
+                    expect(result).to.include( { id: 1000, username:"test",sex: "male"  } );
+                    done();
+                });
+        });
+        it('should return one of the informations in an array by fuzzy search', function(done) {
+            request(server)
+                .get('/informations/f/t')
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(1);
+                    let result = _.map(res.body, (inf) => {
+                        return { id: inf.id,
+                            username: inf.username,
+                            sex: inf.sex}
+                    });
+                    expect(result).to.include({username:"test",sex: "male"});
+                    done();
+                });
+        });
+    });
     describe('PUT /informations/:username', () => {
         it('should return a message and the information amountofmessage by 1', function(done) {
-            chai.request(server)
+            request(server)
                 .put('/informations/test')
                 .end(function(err, res) {
                     expect(res).to.have.status(200);
@@ -61,10 +84,10 @@ describe('Informations', function (){
                 });
         });
     });
-    describe('DELETE /informations/:id', function () {
+    describe('DELETE /informations/:username', function () {
         it('should return Information Successfully Deleted!', function(done) {
-            chai.request(server)
-                .delete('/informations/1000')
+            request(server)
+                .delete('/informations/test')
                 .end(function(err, res) {
                     expect(res).to.have.status(200);
                     let information = res.body.message;
@@ -74,8 +97,8 @@ describe('Informations', function (){
         });
     });
     describe('DELETE /informations', function () {
-        it('should return Information Successfully Deleted!', function(done) {
-            chai.request(server)
+        it('should return All of the Informations Successfully Deleted!', function(done) {
+            request(server)
                 .delete('/informations')
                 .end(function(err, res) {
                     expect(res).to.have.status(200);
@@ -84,5 +107,6 @@ describe('Informations', function (){
                     done();
                 });
         });
+
     });
 });
